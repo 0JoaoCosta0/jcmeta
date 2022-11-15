042fa0018f23ba4f87205e08ca4776bce9c09ef6
0JoaoCosta0 <joao.costa0347@gmail.com>
2022-11-15 11:28:39 -0300

    Project Created

---
+++ backend/src/test/java/com/costa/jcmeta/JcmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costa.jcmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
